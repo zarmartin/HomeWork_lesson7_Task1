d291ed7868d54e10ee9d868644e7ab1cace3971f
Martin Zarutski <martinzar5@gmail.com>
2023-06-23 15:39:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Введите 6 чисел:");
+
+int[] numbers = new int[6];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Array.Sort(numbers);
+
+Array.Reverse(numbers);
+
+Console.WriteLine("Результат отсортирован по убыванию:");
+
+foreach (int i in numbers)
+{
+    
+    Console.WriteLine(i);
+}
